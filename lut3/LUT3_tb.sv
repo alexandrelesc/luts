69f1397bc62c9e553b58c761b1f50b9977d40eb1
alexandrelesc <93800383+alexandrelesc@users.noreply.github.com>
2024-11-06 13:07:34 -0300

    Update LUT3_tb.sv

--- lut3/LUT3_tb.sv
+++ lut3/LUT3_tb.sv
@@ -2,48 +2,48 @@ module LUT3_tb;
   logic O, I0, I1, I2;
   
   // instantiating the module to map connections
-  LUT3 LUT3_INST( .I0(I0), .I1(I1), .I2(I2), .O(O) );
+  LUT3 #(.INIT(8'h0A)) LUT3_INST(.I0(I0), .I1(I1), .I2(I2), .O(O));
   
   initial
     begin
-      I0 = 1'b0;
-      I1 = 1'b0;
       I2 = 1'b0;
+      I1 = 1'b0;
+      I0 = 1'b0;
 
       #5
-      I0 = 1'b0;
-      I1 = 1'b0;
-      I2 = 1'b1;    
-      
-      #5
-      I0 = 1'b0;
-      I1 = 1'b1;
       I2 = 1'b0;
+      I1 = 1'b0;
+      I0 = 1'b1;    
       
       #5
+      I2 = 1'b0;
+      I1 = 1'b1;
       I0 = 1'b0;
-      I1 = 1'b1;
-      I2 = 1'b1;
       
       #5
-      I0 = 1'b1;
-      I1 = 1'b0;
       I2 = 1'b0;
+      I1 = 1'b1;
+      I0 = 1'b1;
       
       #5
-      I0 = 1'b1;
+      I2 = 1'b1;
       I1 = 1'b0;
-      I2 = 1'b1;
+      I0 = 1'b0;
       
       #5
+      I2 = 1'b1;
+      I1 = 1'b0;
       I0 = 1'b1;
-      I1 = 1'b1;
-      I2 = 1'b0;
       
       #5
-      I0 = 1'b1;
-      I1 = 1'b1;
       I2 = 1'b1;
+      I1 = 1'b1;
+      I0 = 1'b0;
+      
+      #5
+      I2 = 1'b1;
+      I1 = 1'b1;
+      I0 = 1'b1;
             
     end
   

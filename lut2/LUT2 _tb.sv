5186f4d3003ab570665b22d7997a47eb942aaccc
alexandrelesc <93800383+alexandrelesc@users.noreply.github.com>
2024-11-06 13:06:19 -0300

    Update LUT2 _tb.sv

--- lut2/LUT2 _tb.sv
+++ lut2/LUT2 _tb.sv
@@ -1,33 +1,34 @@
 module LUT2_tb;
   logic O, I0, I1;
   
-  // instantiating the module to map connections
-  LUT2 LUT2_INST( .I0(I0), .I1(I1), .O(O) );
+  // instantiating the module to map connections 4'b0101
+  LUT2 #(.INIT(4'h8)) LUT2_INST( .I0(I0), .I1(I1), .O(O) );
   
   initial
     begin
-      I0 = 1'b0;
       I1 = 1'b0;
+      I0 = 1'b0;
 
       #5
-      I0 = 1'b0;
-      I1 = 1'b1;      
-      
-      #5
-      I0 = 1'b1;
       I1 = 1'b0;
+      I0 = 1'b1;      
       
       #5
-      I0 = 1'b1;
       I1 = 1'b1;
+      I0 = 1'b0;
+      
+      #5
+      I1 = 1'b1;
+      I0 = 1'b1;
     end
   
     initial
     begin
-      $display("                Tempo   Entradas LUT2    Saídas");
+      $display("                Tempo   Entradas LUT    Saídas");
       $display("                         IO    I1       O");
       $display("                ====   ============  =======");
       $monitor($time,"     %b      %b       %b", I0, I1, O);
     end
   
 endmodule
+
